1bf00d06a7563bc03588337e0b27ff34b8a196a7
fkamag <fabiokamagian@gmail.com>
2024-01-24 15:50:42 -0300

    fix mensagem resultado final

--- src/Principal.java
+++ src/Principal.java
@@ -1,34 +1,101 @@
+import java.util.Scanner;
+
 public class Principal {
 
+  static Scanner scanner = new Scanner(System.in);
+  static GerenciamentoVotacao votacao = new GerenciamentoVotacao();
+
   public static void main(String[] args) {
-    GerenciamentoVotacao votacao = new GerenciamentoVotacao();
+    menu1();
+    menu2();
+    menu3();
+  }
 
-    votacao.cadastrarPessoaCandidata("Maria", 12345);
-    votacao.cadastrarPessoaCandidata("José", 12345);
-    votacao.cadastrarPessoaCandidata("José", 12346);
+  public static void menu1() {
 
-    System.out.println("Candidatos");
-    for (PessoaCandidata candidato : votacao.pessoasCandidatas) {
-      System.out.printf("Nome: %s\t número: %d\n", candidato.getNome(), candidato.getNumero());
+    while (true) {
+      System.out.println("Cadastrar pessoa candidata?");
+      System.out.println("1 - Sim");
+      System.out.println("2 - Não");
+      System.out.println("Entre com o número correspondente à opção desejada:");
+      String option1 = scanner.next();
+      if (option1.equals("1")) {
+        cadatrarCandidato();
+      } else if (option1.equals("2")) {
+        break;
+      } else {
+        System.out.println("Opção inválida");
+      }
     }
+  }
 
-    votacao.cadastrarPessoaEleitora("Maria", "123.456.789-10");
-    votacao.cadastrarPessoaEleitora("José", "123.456.789-10");
-    votacao.cadastrarPessoaEleitora("José", "123.456.789-00");
+  public static void cadatrarCandidato() {
+    System.out.println("Entre com o nome da pessoa candidata: ");
+    String nome = scanner.next();
+    System.out.println("Entre com o número da pessoa candidata:");
+    int numero = scanner.nextInt();
+    votacao.cadastrarPessoaCandidata(nome, numero);
+  }
 
-    System.out.println("Eleitores");
-    for (PessoaEleitora eleitor : votacao.pessoasEleitoras) {
-      System.out.printf("Nome: %s\t cpf: %s\n", eleitor.getNome(), eleitor.getCpf());
+  public static void menu2() {
+
+    while (true) {
+      System.out.println("Cadastrar pessoa eleitora?");
+      System.out.println("1 - Sim");
+      System.out.println("2 - Não");
+      System.out.println("Entre com o número correspondente à opção desejada:");
+      String option2 = scanner.next();
+      if (option2.equals("1")) {
+        cadastrarEleitor();
+      } else if (option2.equals("2")) {
+        break;
+      } else {
+        System.out.println("Opção inválida");
+      }
     }
+  }
 
-    votacao.votar("123", 123);
-    votacao.votar("123.456.789-10", 12345);
-    votacao.votar("123.456.789-10", 12345);
-    votacao.votar("123.456.789-00", 0);
+  public static void cadastrarEleitor() {
+    System.out.println("Entre com o nome da pessoa eleitora: ");
+    String nome = scanner.next();
+    System.out.println("Entre com o cpf da pessoa eleitora:");
+    String cpf = scanner.next();
+    votacao.cadastrarPessoaEleitora(nome, cpf);
+  }
 
+  public static void menu3() {
 
-    votacao.mostrarResultado();
+    label:
+    while (true) {
+      System.out.println("Entre com o número correspondente à opção desejada:");
+      System.out.println("1 - Votar");
+      System.out.println("2 - Resultado Parcial");
+      System.out.println("3 - Finalizar Votação");
+      String option3 = scanner.next();
+      switch (option3) {
+        case "1":
+          votar();
+          break;
+        case "2":
+          votacao.mostrarResultado();
+          break;
+        case "3":
+          System.out.println("RESULTADO FINAL");
+          votacao.mostrarResultado();
+          break label;
+        default:
+          System.out.println("Opção inválida");
+          break;
+      }
+    }
+  }
 
+  public static void votar() {
+    System.out.println("Entre com o cpf da pessoa eleitora:");
+    String cpf = scanner.next();
+    System.out.println("Entre com o número da pessoa candidata:");
+    int numero = scanner.nextInt();
+    votacao.votar(cpf, numero);
   }
 
 }
